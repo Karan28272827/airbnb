db21e82ee4e5b2bcafc7cce79c2a29b949450ceb
Karan Paigude <paigudekaran2827@gmail.com>
2024-07-16 17:40:39 +0530

    pass object as props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from "react"
 
-export default function Card(props) {
+export default function Card(props,item) {
     return (
         <div className="card">
-            {props.openSpots === 0 &&<div className="card--badge">SOLD OUT</div>}
-            <img src={`../images/${props.img}`} className="card--image" />
+            {props.item.openSpots === 0 &&<div className="card--badge">SOLD OUT</div>}
+            <img src={`../images/${props.item.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="/images/Star 1.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p>{props.item.title}</p>
+            <p><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 } 
